9130f594ab75398adfb16beef925b8196ccc8955
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2023-01-03 14:57:05 -0800

    VisualProgramming

---
+++ Arrays/Arrays/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int[] sayi = new int[5];
+            int toplam = 0;
+            for (int j=0;j<sayi.Length;j++)
+            {
+                sayi[j] = rnd.Next(0,50);
+                toplam = toplam + sayi[j];
+                if (toplam > 100)
+                {
+                    MessageBox.Show("Toplam 100'den Büyüktür.");
+                    break;
+                }
+                else
+                {
+                    
+                }
+                
+            }
+            label1.Text = toplam.ToString();
+            MessageBox.Show("Sonuc:" + toplam);
+            
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = { 5, 6, 7, 8 };
+            int toplam = 0;
+            foreach(int sayi in sayilar)
+            {
+                if (sayi % 2 == 0)
+                {
+                    listBox2.Items.Add(sayi);
+                }
+                else
+                {
+                    listBox3.Items.Add(sayi);
+                }
+            }
+            MessageBox.Show("Sonuc:" + toplam);
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -127,3 +127,6 @@ yüzde seçilirse program uyarı veriyor.
 **13.Hafta Ödev**
 - For-While-Do/While-Foreach döngüleri ile ilgili örnek yapılacak.
 - Askerlik Sorgulaması ve Basamak Sayısı Bulma Yapıldı.
+**14.Hafta**
+03.01.2023
+- Diziler(Arrays) ile ilgili örnekler yapıldı. 
